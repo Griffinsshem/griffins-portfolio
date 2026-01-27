dc91b3f8f4a254a1292aa132036fab854ee42deb
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2026-01-27 15:06:47 +0300

    feat(projects): redesign projects section with premium UI and clearer case presentation

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,11 @@
 import Image from "next/image";
 import HomeSection from "./components/Homepage";
-import Projects from "./components/Project";
 import Contact from "./components/Contact";
 
 export default function Home() {
   return (
     <div>
       <HomeSection />
-      <Projects />
       <Contact />
     </div>
   );
