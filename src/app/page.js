c700dcd66d0433ad8d0cec4cfe67ca4b752c3f68
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2026-01-28 07:58:55 +0300

    style(contact): add contact section with recruiter-focused messaging and interactive elements

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,10 @@
 import Image from "next/image";
 import HomeSection from "./components/Homepage";
-import Contact from "./components/Contact";
 
 export default function Home() {
   return (
     <div>
       <HomeSection />
-      <Contact />
     </div>
   );
 }
